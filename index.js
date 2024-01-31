c693aa38ac7597bd83ecdbf99601b82b9b040ca2
hello1964 <69828823+hello1964@users.noreply.github.com>
2024-01-31 18:38:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require('express')
+const app = express()
+
 console.log("The start of something beautiful")
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
