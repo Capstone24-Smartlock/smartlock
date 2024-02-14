077e5defbcdaf0dc081bd9b8e7879972e199efae
hello1964 <DylanPusilo@gmail.com>
2024-02-14 08:00:05 -0500

    H

--- openlock.js
+++ openlock.js
@@ -1,14 +1,14 @@
 const rpio = require("rpio")
 
-var motor = rpio.open(25, rpio.OUTPUT, rpio.LOW)
+var motor = rpio.open(22, rpio.OUTPUT, rpio.LOW)
 
 var on = true
 
 setInterval(function() {
     if (on) {
-        rpio.write(25, rpio.HIGH)
+        rpio.write(22, rpio.HIGH)
     } else {
-        rpio.write(25, rpio.LOW)
+        rpio.write(22, rpio.LOW)
     }
     on = !on
     console.log("Run")
