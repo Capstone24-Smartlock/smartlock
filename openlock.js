0731ea9c58d0fdb55f6e9a595768f5b2d6c6e3c2
hello1964 <DylanPusilo@gmail.com>
2024-02-14 07:49:04 -0500

    Speaker

--- openlock.js
+++ openlock.js
@@ -1,8 +1,15 @@
 const rpio = require("rpio")
 
-var motor = rpio.open(22, rpio.OUTPUT, 0)
+var motor = rpio.open(22, rpio.OUTPUT, rpio.LOW)
+
+var on = true
 
 setInterval(function() {
-    console.log("Working?")
-    rpio.write(22, 1)
+    if (on) {
+        rpio.write(22, rpio.HIGH)
+    } else {
+        rpio.write(22, rpio.LOW)
+    }
+    on = !on
+    console.log("Run")
 }, 1000)
