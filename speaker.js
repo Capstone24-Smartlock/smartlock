2a5d8a4f9eadb47089df7a0e014629f01cabb322
hello1964 <DylanPusilo@gmail.com>
2024-02-15 07:50:14 -0500

    Fixed maybe

--- speaker.js
+++ speaker.js
@@ -2,9 +2,11 @@ const Gpio = require('onoff').Gpio
 const speaker = new Gpio(25, "out")
 
 setInterval(function() {
-    if (speaker.readSync == 0) {
+    let speakerValue = speaker.readSync()
+    if (speakerValue == 0) {
         speaker.writeSync(1)
     } else {
         speaker.writeSync(0)
     }
-})
+    console.log(speakerValue)
+}, 1000)
