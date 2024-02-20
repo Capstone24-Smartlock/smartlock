02978e3264f12092469e6177c60f692a4dae145d
hello1964 <DylanPusilo@gmail.com>
2024-02-20 18:00:22 -0500

    Fixed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.static("views"))
+app.use(express.json())
 
 app.get("^/$|/index(.html)?", function(req, res) {
   res.sendFile(path.join(__dirname, "/views/index.html"))
