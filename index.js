6051b31b6744671a6675ed46ff02605757d7c997
hello1964 <DylanPusilo@gmail.com>
2024-02-20 20:12:59 -0500

    battery

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const path = require("path")
+const dgram = require("dgram")
 const express = require('express')
 const app = express()
+
+const battery = dgram.createSocket("udp4")
+battery.send("get battery", 8421, "127.0.0.1", function(err, bytes) {
+  if (err) {
+    console.log(err)
+  }
+  console.log(bytes)
+  battery.close()
+})
 
 app.use(express.static("views"))
 app.use(express.json())
