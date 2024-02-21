f4810a39146f2297dc3c5e4a405bc8b962bd4365
hello1964 <DylanPusilo@gmail.com>
2024-02-20 20:35:40 -0500

    KJSADJSD

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ battery.connect(8423, "127.0.0.1", function() {
   battery.write("get battery")
 })
 battery.on("data", function(data) {
-  console.log(buffer.toString(data))
+  console.log(data.toString())
 })
 battery.on("close", function() {
   console.log("Battery connection closed")
