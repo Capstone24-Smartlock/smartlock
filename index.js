8a387a0303019418f6c9db64dfc6ca8d657a7809
hello1964 <DylanPusilo@gmail.com>
2024-02-22 07:56:52 -0500

    Oh boy

--- index.js
+++ index.js
@@ -6,53 +6,51 @@ const net = require("net")
 const express = require('express')
 const app = express()
 
-raspi.init(async function() {
-  const motor = new pwm.SoftPWM(5)
-  motor.write(0.05)
-  console.log("0.05")
-  await sleep(1000)
-  motor.write(0.1)
-  console.log("0.1")
-})
+raspi.init(main)
 
 function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
+async function main() {
+  const motor = new pwm.SoftPWM(5)
 
-app.use(express.static("views"))
-app.use(express.json())
+  app.use(express.static("views"))
+  app.use(express.json())
 
-app.get("^/$|/index(.html)?", function(req, res) {
-  res.sendFile(path.join(__dirname, "/views/index.html"))
-})
-
-app.get("/log(.html)?", function(req, res) {
-  res.sendFile(path.join(__dirname, "views/log/log.html"))
-})
-
-app.get("/battery", function(req, res) {
-  const battery = new net.Socket()
-  battery.connect(8423, "127.0.0.1", function() {
-    battery.write("get battery")
+  app.get("^/$|/index(.html)?", function(req, res) {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
   })
-  battery.on("data", function(data) {
-    let level = (parseFloat(data.toString().split(" ")[1])/100).toString()
-    res.send(level)
-    battery.destroy()
+
+  app.get("/log(.html)?", function(req, res) {
+    res.sendFile(path.join(__dirname, "views/log/log.html"))
   })
-})
 
-app.post("^/$|/index(.html)?", function(req, res) {
-  switch (req.body.req) {
-    case "lock":
-      console.log("Lock")
-    case "unlock":
-      console.log("Unlock")
-  }
-})
+  app.get("/battery", function(req, res) {
+    const battery = new net.Socket()
+    battery.connect(8423, "127.0.0.1", function() {
+      battery.write("get battery")
+    })
+    battery.on("data", function(data) {
+      let level = (parseFloat(data.toString().split(" ")[1])/100).toString()
+      res.send(level)
+      battery.destroy()
+    })
+  })
 
-app.listen(8080, function() {
-  console.log("Let's go")
-})
+  app.post("^/$|/index(.html)?", function(req, res) {
+    switch (req.body.req) {
+      case "lock":
+        console.log("Lock")
+        motor.write(0.1)
+      case "unlock":
+        console.log("Unlock")
+        motor.write(0.05)
+    }
+  })
+
+  app.listen(8080, function() {
+    console.log("Let's go")
+  })
+}
