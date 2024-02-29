64769cd2b7a91df764eed10ec71dee949aeff851
hello1964 <DylanPusilo@gmail.com>
2024-02-29 12:21:50 -0500

    Event logger

--- views/log/log.js
+++ views/log/log.js
@@ -1,3 +1,16 @@
 var events = document.getElementById("events")
 
-//fetch("/events")
+fetch("/events").then(function(res) {
+    return res.json()
+}).then(function(data) {
+    for (i = data.date.length; i >= 0; i++) {
+        let row = document.createElement("tr")
+        let items = [data.date[i], data.time[i], data.event[i]]
+        for (let s = 0; s < 3; s++) {
+            let elem = document.createElement("td")
+            elem.innerHTML = items[s]
+            row.appendChild(elem)
+        }
+        events.appendChild(row)
+    }
+})
