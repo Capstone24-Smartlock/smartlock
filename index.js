7357e3a5b73f9804c0cc5e77300a15dd4b91b32e
hello1964 <DylanPusilo@gmail.com>
2024-03-01 11:40:41 -0500

    Data

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ async function getBattery(req) {
   })
   return await new Promise(function(resolve, reject) {
     battery.on("data", function(data) {
+      console.log(data)
       resolve(data)
     })
   }).then(function(data) {
