bc01af5771e9556e87bb8f9a2ebc1aef09818737
hello1964 <DylanPusilo@gmail.com>
2024-03-01 07:43:35 -0500

    New Log

--- views/log/log.js
+++ views/log/log.js
@@ -1,16 +1,56 @@
 var events = document.getElementById("events")
 
+class LogEvent {
+    constructor(date, time, event) {
+        this.date = date
+        this.time = time
+        this.event = event
+    }
+
+    get message() {
+        switch (this.event) {
+            case 0:
+                return "Lock was locked"
+            case 1:
+                return "Lock was unlocked"
+        }
+    }
+
+    get color() {
+        let colors = {
+            black: [1],
+            green: [0],
+            red: [],
+        }
+        let colorList = Object.keys(colors)
+        for (let i = 0; i < colorList.length; i++) {
+            if (colors[colorList[i]].includes(this.event)) {
+                return colorList[i]
+            }
+        }
+    }
+
+    get row() {
+        let row = document.createElement("tr")
+        let items = [this.date, this.time, this.message]
+        for (let i = 0; i < items.length; i++) {
+            let elem = document.createElement("td")
+            elem.style.color = this.color
+            elem.innerHTML = items[i]
+            row.appendChild(elem)
+        }
+        return row
+    }
+}
+
+var eve = []
+
 fetch("/events").then(function(res) {
     return res.json()
 }).then(function(data) {
     for (i = data.date.length - 1; i >= 0; i--) {
-        let row = document.createElement("tr")
-        let items = [data.date[i], data.time[i], data.event[i]]
-        for (let s = 0; s < 3; s++) {
-            let elem = document.createElement("td")
-            elem.innerHTML = items[s]
-            row.appendChild(elem)
-        }
-        events.appendChild(row)
+        let event = new LogEvent(data.date[i], data.time[i], data.event[i])
+        eve.push(event)
+        events.appendChild(event.row)
     }
 })
