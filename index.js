bcacbdf5b0bf93f1c1ffd286e386c1474ef9e2ff
hello1964 <DylanPusilo@gmail.com>
2024-03-01 12:05:25 -0500

    Float

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ async function batteryProperties(req) {
       resolve(data.toString())
     })
   }).then(function(data) {
-    console.log(data.split(" ")[1])
+    console.log(parseFloat(data.split(" ")[1]))
     return data.split(" ")[1]
   })
 }
