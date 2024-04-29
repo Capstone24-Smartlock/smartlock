d3e670708d5523c09615b227d48368bd2cd56227
hello1964 <DylanPusilo@gmail.com>
2024-04-29 07:42:19 -0400

    No Database

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { Socket } = require("dgram")
 const app = express()
 const WebSocket = require("ws")
 const libcamera = require("libcamera").libcamera
-const mariadb = require("mariadb")
+//const mariadb = require("mariadb")
 
 //This creates the class that allows the server code to communicate with the front end through a WebSocket, allowing for real-time updates to occur on the front end. For example, when the lock is closed, the lock button will update to show a locked lock, or when the alarm is triggered, the “stop alarm” button will appear.
 class WebSocketAPI {
